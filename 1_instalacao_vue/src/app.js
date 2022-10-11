e9b84871e3c820d395b2725867e5ab5c00a0dbfe
Joao Winkelmann <hmaster.studio@gmail.com>
2022-10-11 08:35:11 -0300

    alterações em tempo real

--- 1_instalacao_vue/src/app.js
+++ 1_instalacao_vue/src/app.js
@@ -2,7 +2,16 @@ const MeuApp = {
     data(){
         return{
             name: "João Vitor", //dado que é chamado pelo html
-            idade: 19
+            idade: 19,
+            input_name: "" // está bindado com o input v-model
+        }
+    },
+    methods: {
+        submitForm(e){
+            e.preventDefault();
+            console.log(this.input_name);
+
+            this.name = this.input_name;
         }
     }
 }
